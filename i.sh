921bc4158e8bc13a47ab86349c59861692ce7149
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-08-25 02:55:21 +0300

    Add files via upload

---
+++ i.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/zsh
+
+# 更新系统时间
+timedatectl set-ntp true
+
+# 分区与格式化
+
+echo "g
+n
+
+
++512M
+t
+1
+n
+
+
+
+w" | fdisk /dev/sda
+
+# 格式化分区
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+
+# 挂载分区
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+# 安装基本包
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd openssh neovim sudo zsh git
+
+# 配置shell
+rm /mnt/etc/skel/.bash*
+sed -i "s|/bin/bash|/usr/bin/zsh|g" /mnt/etc/default/useradd
+sed -i "s|/bin/bash|/usr/bin/zsh|g" /mnt/etc/passwd
+
+# 配置Fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# 下载后续脚本
+for loop in 1 2 3
+do
+    curl -o /mnt/step$loop.sh "https://raw.githubusercontent.com/Kirara17233/config/main/step$loop.sh"
+done
+curl -o /mnt/usr/lib/systemd/system/install.service "https://raw.githubusercontent.com/Kirara17233/config/main/install.service"
+chmod +x /mnt/step*.sh
+sed -i "s|#rootpw|$1|g" /mnt/step*.sh
+sed -i "s|#user|$2|g" /mnt/step*.sh
+sed -i "s|#userpw|$3|g" /mnt/step*.sh
+sed -i "s|#token|$4|g" /mnt/step*.sh
+
+# 开启pacman色彩选项
+sed -i "s|#Color|Color|g" /mnt/etc/pacman.conf
+
+# Chroot
+arch-chroot /mnt /step1.sh
+
+# 重启
+umount /mnt/boot
+umount /mnt
+reboot
---
+++ step2.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/zsh
+
+user=#user
+userpw=#userpw
+
+# 创建交换文件
+dd if=/dev/zero of=/swapfile bs=1M count=8192 status=progress
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+sed -i "7i /swapfile                                       none            swap            defaults        0 0" /etc/fstab
+
+# 新建用户
+useradd -m -G wheel $user
+echo "$user:$userpw" | chpasswd
+groupadd autologin
+gpasswd -a $user autologin
+
+# 安装yay
+su $user <<EOF
+git clone --depth=1 https://aur.archlinux.org/yay.git /home/$user/yay
+cd /home/$user/yay
+makepkg -rsi --noconfirm
+cd ..
+rm -rf /home/$user/yay
+EOF
+
+# 下载vim-plug
+mkdir /etc/xdg/nvim/autoload
+curl -fLo /etc/xdg/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "
+call plug#begin('~/.nvim/plugged')
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'jiangmiao/auto-pairs'
+call plug#end()
+
+nmap <silent> q :q! <cr>
+
+let g:airline_powerline_fonts = 1
+\"let g:airline_theme='deus'
+let g:airline_theme='bubblegum'
+\"let g:airline_theme='minimalist'
+set nu" >> /etc/xdg/nvim/sysinit.vim
+
+# 安装软件
+su $user <<EOF
+yay -S --noconfirm curl wget neofetch xf86-video-vmware xorg-server gtk3 lightdm numlockx xmonad xmonad-contrib rofi ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono noto-fonts-sc nix open-vm-tools jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome
+EOF
+
+systemctl enable lightdm vmtoolsd vmware-vmblock-fuse
+
+# 安装termonad
+git clone --depth=1 https://github.com/cdepillabout/termonad /root/termonad
+cd /root/termonad
+nix-build
+cp /root/termonad/result/bin/termonad /usr/bin/termonad
+
+# 启用自动登录
+sed -i "118i session-setup-script=/usr/bin/numlockx on" /etc/lightdm/lightdm.conf
+sed -i "s|#autologin-user=|autologin-user=$user|g" /etc/lightdm/lightdm.conf
+sed -i "s|#autologin-session=|autologin-session=xmonad|g" /etc/lightdm/lightdm.conf
+
+# 解除
+systemctl disable install
+
+# 清理文件
+rm /usr/lib/systemd/system/install.service
+rm /step*.sh
+
+# 重启
+reboot
