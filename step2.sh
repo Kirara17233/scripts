92f67900bce28b6f1189c55aa7fbdc2e06f84488
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-08-28 07:35:09 +0300

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -55,7 +55,7 @@ EOF
 
 if [ $model -eq 1 ];then
     su $user <<EOF
-    yay -S --noconfirm xf86-video-vmware xorg-server gtk3 lightdm numlockx xmonad xmonad-contrib xmobar rofi ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono noto-fonts-sc nix open-vm-tools jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome
+    yay -S --noconfirm xf86-video-vmware xorg-server xorg-xsetroot gtk3 lightdm numlockx xmonad xmonad-contrib xmobar rofi ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono noto-fonts-sc nix open-vm-tools jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome
 EOF
 
     systemctl enable lightdm vmtoolsd vmware-vmblock-fuse
