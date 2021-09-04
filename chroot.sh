9084868bed10cc98d38419f2512a7a97ebd777fe
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-09-04 06:12:47 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -106,7 +106,8 @@ git clone --depth=1 https://aur.archlinux.org/yay.git /home/$user/yay
 cd /home/$user/yay
 makepkg -rsi --noconfirm
 cd ~
-rm -rf /home/$user/yay"
+rm -rf /home/$user/yay
+EOF"
 
 # Neovim configuration
 run "mkdir /etc/xdg/nvim/autoload"
@@ -116,7 +117,8 @@ run "cp /root/config/archlinux.vim /usr/share/nvim/archlinux.vim"
 # Install GUI packages
 if [ $model -eq 1 ];then
   run "su $user <<EOF
-  yay -S --noconfirm xf86-video-vmware xorg-server xorg-xsetroot breeze-gtk xwallpaper gtk3 picom alsa-utils lightdm numlockx xmonad xmonad-contrib xfce4-panel vala-panel-appmenu-xfce xmobar rofi ttf-meslo-nerd-font-powerlevel10k alacritty ttf-jetbrains-mono noto-fonts-sc open-vm-tools jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome"
+  yay -S --noconfirm xf86-video-vmware xorg-server xorg-xsetroot breeze-gtk xwallpaper gtk3 picom alsa-utils lightdm numlockx xmonad xmonad-contrib xfce4-panel vala-panel-appmenu-xfce xmobar rofi ttf-meslo-nerd-font-powerlevel10k alacritty ttf-jetbrains-mono noto-fonts-sc open-vm-tools jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome
+  EOF"
 
   run "systemctl enable lightdm vmtoolsd vmware-vmblock-fuse"
 
