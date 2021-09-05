3ac5cbe533d6065368ce5d425c50d00922169ba1
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-09-05 06:16:57 +0300

    Update ending.sh

--- ending.sh
+++ ending.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/zsh
 
-run() {
-  echo "$1 2>> /err.info" | zsh
-  if [ "$?" -ne 0 ]; then
-    run $1 /err.info
-  fi
-}
-
-run "cd /etc/termonad"
-run "sudo nix-build"
-run "xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true"
-run "xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true"
-run "sudo rm /ending.sh"
+xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
+xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
+cd /etc/termonad
+sudo nix-build
+sudo rm /ending.sh
