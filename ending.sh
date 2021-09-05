90f1b95512a05224c74d09e661d408c076339ab5
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-09-05 06:45:22 +0300

    Update ending.sh

--- ending.sh
+++ ending.sh
@@ -4,4 +4,5 @@ xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
 cd /etc/termonad
 sudo nix-build
+sudo cp /etc/termonad/result/bin/termonad /usr/bin
 sudo rm /ending.sh
