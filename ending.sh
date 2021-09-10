f2ace29842ba02c87c8fc01efe7b96fecfa9c262
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-09-10 10:42:27 +0300

    Update ending.sh

--- ending.sh
+++ ending.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
+sudo git --git-dir=/etc/termonad/.git --work-tree=/etc/termonad pull
 sudo nix-build /etc/termonad
 sudo cp /etc/termonad/result/bin/termonad /usr/bin
-sudo rm /ending.sh
