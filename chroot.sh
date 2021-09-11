f54f2ffadf129fa25041fdf96d6356c92f960f1a
Kirara17233 <85111701+Kirara17233@users.noreply.github.com>
2021-09-11 11:18:08 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -127,7 +127,7 @@ if [ $model -eq 1 ];then
   run "su $user << EOF
   yay -S --noconfirm xf86-video-vmware open-vm-tools alsa-utils numlockx gobject-introspection nix\
       xorg-server xorg-xsetroot xwallpaper wmctrl xorg-xrandr gtk3 lightdm xmonad xmonad-contrib picom xmobar xfce4-panel vala-panel-appmenu-registrar vala-panel-appmenu-xfce rofi\
-      xxd-standalone ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono noto-fonts-sc jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome bash-pipes cmatrix
+      ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono noto-fonts-sc jdk-openjdk jetbrains-toolbox visual-studio-code-bin google-chrome bash-pipes cmatrix
 EOF
 "
   run "git clone --depth=1 https://github.com/cdepillabout/termonad /etc/termonad"
